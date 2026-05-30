ac7302c340127a69d732fda4d689e9da7abf4d8b
thotamahathi <thotamahathi1@gmail.com>
2026-05-30 22:08:32 +0530

    Time: 428 ms (52%), Space: 285.3 MB (35%) - LeetHub

--- 3161-block-placement-queries/3161-block-placement-queries.java
+++ 3161-block-placement-queries/3161-block-placement-queries.java
@@ -1,63 +1,62 @@
 class Solution {
 
-    private int[] bt;
+    private int[] seg;
 
-    private void update(int x, int v) {
-        for (; x < bt.length; x += x & -x) {
-            bt[x] = Math.max(bt[x], v);
+    private void update(int idx, int val, int p, int l, int r) {
+        if (l == r) {
+            seg[p] = val;
+            return;
         }
+        int mid = (l + r) >> 1;
+        if (idx <= mid) {
+            update(idx, val, p << 1, l, mid);
+        } else {
+            update(idx, val, (p << 1) | 1, mid + 1, r);
+        }
+        seg[p] = Math.max(seg[p << 1], seg[(p << 1) | 1]);
     }
 
-    private int query(int x) {
+    private int query(int L, int R, int p, int l, int r) {
+        if (L <= l && r <= R) {
+            return seg[p];
+        }
+        int mid = (l + r) >> 1;
         int res = 0;
-        for (; x > 0; x -= x & -x) {
-            res = Math.max(res, bt[x]);
+        if (L <= mid) {
+            res = Math.max(res, query(L, R, p << 1, l, mid));
+        }
+        if (R > mid) {
+            res = Math.max(res, query(L, R, (p << 1) | 1, mid + 1, r));
         }
         return res;
     }
 
     public List<Boolean> getResults(int[][] queries) {
         int mx = 50000;
-
+        seg = new int[mx << 2];
         TreeSet<Integer> st = new TreeSet<>();
         st.add(0);
         st.add(mx);
+        update(mx, mx, 1, 0, mx);
+        List<Boolean> ans = new ArrayList<>();
 
         for (int[] q : queries) {
             if (q[0] == 1) {
-                st.add(q[1]);
-            }
-        }
-
-        bt = new int[mx + 1];
-
-        int pre = 0;
-        for (int x : st) {
-            if (x == 0) continue;
-            update(x, x - pre);
-            pre = x;
-        }
-
-        List<Boolean> ans = new ArrayList<>();
-        for (int i = queries.length - 1; i >= 0; i--) {
-            int[] q = queries[i];
-            if (q[0] == 2) {
                 int x = q[1];
-                int sz = q[2];
-                int preVal = Optional.ofNullable(st.floor(x)).orElse(0);
-                int maxSpace = query(preVal);
-                maxSpace = Math.max(maxSpace, x - preVal);
-                ans.add(maxSpace >= sz);
+                int r = Optional.ofNullable(st.ceiling(x + 1)).orElse(mx);
+                int l = Optional.ofNullable(st.floor(x - 1)).orElse(0);
+                update(x, x - l, 1, 0, mx);
+                update(r, r - x, 1, 0, mx);
+                st.add(x);
             } else {
                 int x = q[1];
-                int preVal = Optional.ofNullable(st.lower(x)).orElse(0);
-                int nxt = Optional.ofNullable(st.higher(x)).orElse(mx);
-                update(nxt, nxt - preVal);
-                st.remove(x);
+                int sz = q[2];
+                int pre = Optional.ofNullable(st.floor(x)).orElse(0);
+                int max_space = query(0, pre, 1, 0, mx);
+                max_space = Math.max(max_space, x - pre);
+                ans.add(max_space >= sz);
             }
         }
-
-        Collections.reverse(ans);
         return ans;
     }
 }
